091095d4154183c4272e1335bac99e3ad8626f11
Michael Berg <michael.berg@zalf.de>
2015-01-06 16:30:19 +0100

    dev dump
    added explicit year selector for annual year creation

--- hoplon/project.clj
+++ hoplon/project.clj
@@ -3,5 +3,6 @@
                  [tailrecursion/boot.task   "2.1.1"]
                  [tailrecursion/hoplon      "5.5.1"]
                  [org.clojure/clojurescript "0.0-2156"]
-                 [cljs-ajax "0.2.3"]]
+                 [cljs-ajax "0.2.3"]
+                 [com.andrewmcveigh/cljs-time "0.1.5"]]
   :source-paths ["src"])
